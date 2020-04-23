049d4ee22e0dfe956e8a45da06080edbe88e4695
Ioannis Stavrinides <iostavri@microsoft.com>
2020-04-23 17:43:34 +0300

    Updates to Add New Question view (select services, difficulty slider, adding/removing multiple endpoints and associated answers)

--- AzureChallenge.UI/Areas/Administration/Controllers/QuestionController.cs
+++ AzureChallenge.UI/Areas/Administration/Controllers/QuestionController.cs
@@ -10,6 +10,15 @@ using AzureChallenge.UI.Areas.Administration.Models.Questions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using AzureChallenge.Interfaces.Providers.Questions;
+using System.Net;
+using System.IO;
+using System.IO.Pipes;
+using Microsoft.Extensions.Logging.Abstractions;
+using System.Net.Http;
+using System.Configuration;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace AzureChallenge.UI.Areas.Administration.Controllers
 {
@@ -18,12 +27,37 @@ namespace AzureChallenge.UI.Areas.Administration.Controllers
     {
         private IQuestionProvider<ACM.AzureChallengeResult, ACMQ.Question> questionProvider;
         private IMapper mapper;
+        private IConfiguration configuration;
 
-        public QuestionController(IQuestionProvider<ACM.AzureChallengeResult, ACMQ.Question> questionProvider, IMapper mapper)
+        public QuestionController(IQuestionProvider<ACM.AzureChallengeResult,
+                                    ACMQ.Question> questionProvider,
+                                    IMapper mapper,
+                                    IConfiguration configuration)
         {
             this.questionProvider = questionProvider;
             this.mapper = mapper;
+            this.configuration = configuration;
         }
+
+        public class AzureServiceListReponse
+        {
+            public AzureServiceClass[] Classes { get; set; }
+        }
+
+        public class AzureServiceClass
+        {
+            public string name { get; set; }
+            public string scope { get; set; }
+            public Service[] services { get; set; }
+        }
+
+        public class Service
+        {
+            public string name { get; set; }
+            public string url { get; set; }
+            public string description { get; set; }
+        }
+
 
         public IActionResult Index()
         {
@@ -33,7 +67,33 @@ namespace AzureChallenge.UI.Areas.Administration.Controllers
         [HttpGet]
         public async Task<IActionResult> AddNew()
         {
-            return View(new AddNewQuestionViewModel());
+            var client = new HttpClient();
+            var response = await client.GetAsync(configuration["Endpoints:AzureServicesEnpoint"]);
+            var azureServiceList = new List<string>();
+
+            if (response != null)
+            {
+                var serviceListStr = await response.Content.ReadAsStringAsync();
+                var serviceList = JsonConvert.DeserializeObject<List<AzureServiceClass>>(serviceListStr);
+                var azureServices = serviceList.Where(p => p.name == "Azure").FirstOrDefault();
+                
+                if (azureServices != null)
+                {
+                    foreach (var service in azureServices.services)
+                    {
+                        azureServiceList.Add(service.name);
+                    }
+                }
+            }
+
+            var model = new AddNewQuestionViewModel()
+            {
+                Uris = new List<AddNewQuestionViewModel.UriList>(),
+                Answers = new List<AddNewQuestionViewModel.AnswerList>(),
+                AzureServicesList = azureServiceList
+            };
+
+            return View(model);
         }
 
         [HttpPost]
